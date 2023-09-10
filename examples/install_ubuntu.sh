49608e8ca779c04615a29fc23d86445bfb52b8e3
rniwase <rniwase@users.noreply.github.com>
2023-09-10 17:04:45 +0900

    update build dependencies

--- examples/install_ubuntu.sh
+++ examples/install_ubuntu.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo -e "Installing dependency"
+echo -e "Installing dependencies"
 DEBIAN_FRONTEND=noninteractive sudo apt install -y \
 	build-essential \
 	clang \
@@ -13,57 +13,56 @@ DEBIAN_FRONTEND=noninteractive sudo apt install -y \
 	tcl-dev \
 	libffi-dev \
 	git \
-	mercurial \
 	graphviz \
 	xdot \
 	pkg-config \
-	python2 \
 	python3 \
+	libboost-system-dev \
+	libboost-python-dev \
+	libboost-filesystem-dev \
+	zlib1g-dev \
+	cmake \
+	libboost-all-dev \
+	libeigen3-dev \
 	libftdi-dev \
 	python3-dev \
-	python3-pip \
-	libboost-all-dev \
-	make \
-	cmake \
-	libeigen3-dev
+	python3-pip
 
 mkdir temp
 pushd temp
 
 echo -e "Installing icestorm"
-git clone https://github.com/YosysHQ/icestorm.git icestorm
+mkdir icestorm
 pushd icestorm
-git checkout 45f5e5f3889afb07907bab439cf071478ee5a2a5
+git init
+git remote add origin https://github.com/YosysHQ/icestorm.git
+git fetch --depth 1 origin d20a5e9001f46262bf0cef220f1a6943946e421d
+git reset --hard FETCH_HEAD
 make -j$(nproc)
 sudo make install
 popd
 
 echo -e "Installing nextpnr"
-git clone https://github.com/YosysHQ/nextpnr nextpnr
+git clone https://github.com/YosysHQ/nextpnr.git -b nextpnr-0.6 --depth 1
 pushd nextpnr
-git checkout nextpnr-0.5
 cmake -DARCH=ice40 -DCMAKE_INSTALL_PREFIX=/usr/local .
 make -j$(nproc)
 sudo make install
 popd
 
 echo -e "Installing yosys"
-git clone https://github.com/YosysHQ/yosys.git yosys
+git clone https://github.com/YosysHQ/yosys.git -b yosys-0.33 --depth 1
 pushd yosys
-git checkout yosys-0.25
 make -j$(nproc)
 sudo make install
 popd
 
 echo -e "Installing gbdk-2020"
-wget https://github.com/gbdk-2020/gbdk-2020/releases/download/4.1.1/gbdk-linux64.tar.gz
+wget https://github.com/gbdk-2020/gbdk-2020/releases/download/4.2.0/gbdk-linux64.tar.gz
 sudo tar -axvf gbdk-linux64.tar.gz -C /opt
-export PATH=$PATH:/opt/gbdk/bin
-echo "export PATH=\$PATH:/opt/gbdk/bin" >> ~/.bashrc
-
-echo -e "Installing pyftdi"
 popd
-pip3 install -r ../programmer/requirements.txt
 
 rm -rf temp
 
+echo -e "Installing pyftdi"
+pip3 install -r ../programmer/requirements.txt
